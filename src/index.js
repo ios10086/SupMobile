60c2f8fe1857537268d313e9de84fd05f53f8433
hcm <huchenming@supcon.com>
2021-04-07 17:32:22 +0800

    增加路由跳转

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, withRouter, Switch, Redirect } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import routes from './router';
 
-const App = () => (
-    <div>111222</div>
+const ANIMATION_MAP = {
+    PUSH: 'forward',
+    POP: 'back'
+};
+
+function RouteWithSubRoutes(route) {
+    return (
+      <Route exact path={route.path} component={route.component}/>
+    );
+  }
+
+const Routes = withRouter(({ location, history }) => (
+    <TransitionGroup
+        className={'router-wrapper'}
+        childFactory={child => React.cloneElement(
+            child,
+            { classNames: ANIMATION_MAP[history.action] }
+        )}
+    >
+        <CSSTransition
+            timeout={500}
+            key={location.pathname}
+        >
+            <Switch location={location}>
+                {routes.map((route, i) => (
+                    <RouteWithSubRoutes key={`route_${i + 1}`} {...route} />
+                ))}
+                <Redirect to="/" />
+            </Switch>
+        </CSSTransition>
+    </TransitionGroup>
+));
+
+ReactDOM.render(
+    <BrowserRouter basename='/Warn'>
+        <Routes />
+    </BrowserRouter>,
+    document.getElementById('root')
 );
-
-render(<App />, document.getElementById('root'));
